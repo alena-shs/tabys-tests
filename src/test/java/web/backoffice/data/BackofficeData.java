83c93d1a352ac6f1558fbd2d6c820a88a525ee81
Alena <alenashomanova@gmail.com>
2023-10-06 10:41:39 -0500

    Code refactoring

--- src/test/java/web/backoffice/data/BackofficeData.java
+++ src/test/java/web/backoffice/data/BackofficeData.java
@@ -2,6 +2,6 @@ package web.backoffice.data;
 
 public class BackofficeData {
     public static String login = "a.shomanova",
-            password = "Weare1331!",
+            password = "",
             defaultOtp = "999";
 }
